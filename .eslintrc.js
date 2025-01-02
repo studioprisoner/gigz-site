6565400116f2f10df6d9a70298af5acc98dc3454
Josh Illichmann <josh@studioprisoner.com>
2025-01-02 20:07:22 +1100

    added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended'
+  ],
+  plugins: ['react', '@typescript-eslint'],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'react/no-unescaped-entities': 'warn',
+    // Add other rules as needed
+  },
+}; 
